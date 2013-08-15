ef8715da5bf9ca3ecec903aa022751046a23a124
saltandbeer <raditude28@gmail.com>
2013-08-15 08:14:57 -0400

    Inprogess

--- hc_print.rb
+++ hc_print.rb
@@ -1,20 +1,20 @@
 require 'sinatra'
 require 'erb'
+require 'csv'
 
 get '/' do
   erb :upload
 end
 
-post '/upload' do
-  tempfile = params[:file][:tempfile]
-  filename = params[:file][:filename]
-
+post '/print' do
+  @arr_of_arrs = CSV.read("files/sample.csv").join(", ")
+  return @arr_of_arrs
 end
 
 get '/print' do
-
+  return @arr_of_arrs
 end
-
+	
 not_found do
   "can't find it sorry"
 end
